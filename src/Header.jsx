54f2ed81cfc24a25040b34360f8a9e9c16b4be78
Anjali Jainapure <anjalijainapure@gmail.com>
2024-04-03 15:14:26 +0530

    Add Header Component
    Added Header Component and placed in the App component.

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <>
+      <header className="bg-slate-100 flex  items-center justify-between p-5 ">
+        <button className="bg-blue-300 rounded-xl font-bold py-2 px-5 cursor-auto	">
+          Anjali
+        </button>
+        <p className="font-bold text-xl ">Random Quotes</p>
+        <button className=" bg-blue-300 rounded-xl py-2 px-5 font-bold cursor-auto">
+          Made with Love
+        </button>
+      </header>
+    </>
+  );
+};
+
+export default Header;
