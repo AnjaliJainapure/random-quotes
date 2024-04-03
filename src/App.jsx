9ff6622813f44375dda4074d08b780ed6f1919d6
Anjali Jainapure <anjalijainapure@gmail.com>
2024-04-03 15:50:07 +0530

    Add Footer Component
    Added Footer Component and placed in the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./Footer";
 import Header from "./Header";
 
 export default function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-slate-500 p-5 text-center text-white absolute  bottom-0 w-full">
+        <p>Copyrights © 2024 Random Quotes | All Rights Reserved</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
