536002ac12c72bc8c7235884ca405bc95dec2b9e
Anjali Jainapure <anjalijainapure@gmail.com>
2024-04-04 02:17:54 -0400

    Add API
    Added API to the Body Component.

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 const Body = () => {
+  const [quotes, setQuotes] = useState("View Random Quotes here");
+
+  const getQuotes = () => {
+    setQuotes("Generating...");
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((content) => setQuotes(content.content))
+      .catch((err) => {
+        console.error("Error fetching quotes:", err);
+        setQuotes("Failed to get Quotes");
+      });
+  };
   return (
     <>
       <main className=" flex flex-col justify-center h-screen items-center">
         <p className="font-bold text-xl">Random Quotes are displayed here </p>
-        <button className="bg-gray-100 border border-gray-300 py-7 px-20 shadow-lg  rounded-lg mt-5 mb-5">
-          Quotes found here..
+        <button className="bg-gray-100 border border-gray-300 py-7 px-20 shadow-lg  rounded-lg mt-5 mb-5 cursor-default">
+          <p>{quotes} </p>
         </button>
-        <button className="bg-blue-300 rounded-xl py-2 px-5 font-bold ">
+
+        <button
+          className="bg-blue-300 rounded-xl py-2 px-5 font-bold"
+          onClick={getQuotes}
+        >
           Click me
         </button>
       </main>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,10 @@
 const Header = () => {
+  function getQuotes() {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => content.text)
+      .catch((err) => "Failed to get Quotes");
+  }
   return (
     <>
       <header className="bg-slate-100 flex  items-center justify-between p-5 absolute top-0 w-full">
@@ -6,7 +12,10 @@ const Header = () => {
           Anjali
         </button>
         <p className="font-bold text-xl ">Random Quotes</p>
-        <button className=" bg-blue-300 rounded-xl py-2 px-5 font-bold cursor-auto">
+        <button
+          className=" bg-blue-300 rounded-xl py-2 px-5 font-bold cursor-auto "
+          onClick={getQuotes}
+        >
           Made with Love
         </button>
       </header>
